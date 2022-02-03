c5a44ea02f21a509bcea17752f95ee866f774826
root <root@guest.guest>
2022-02-03 12:18:37 +0000

    update

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ apt-get install -y unzip
 apt-get install -y nginx
 apt-get install -y supervisor
 
+# install oh-my-zsh
+# sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
 cwd_dir=`pwd`
 echo $cwd_dir
 
@@ -13,10 +16,28 @@ mkdir -p ~/web/trojan-go
 cd ~/web/trojan-go
 wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-amd64.zip
 unzip trojan-go-linux-amd64.zip
+chmod +x trojan-go
 
-# download 
+# download cloudreve
 mkdir -p ~/web/cloudreve
 cd ~/web/cloudreve
-wget https://github.com/cloudreve/Cloudreve/releases/download/3.4.2/cloudreve_3.4.2_linux_amd64.tar.gz
-tar -zxvf cloudreve_3.4.2_linux_amd64.tar.gz
+https://github.com/cloudreve/Cloudreve/releases/download/3.4.1/cloudreve_3.4.1_linux_amd64.tar.gz
+tar -zxvf cloudreve_3.4.1_linux_amd64.tar.gz
+chmod +x cloudreve
+
+# copy https crt
+mkdir -p ~/web/https
+cp $cwd_dir/nginx/1.crt ~/web/https
+cp $cwd_dir/nginx/2.key ~/web/https
+cp $cwd_dir/nginx/cloudreve /etc/nginx/sites-enabled/
+service nginx restart
+
+# copy trojan-go config
+cp $cwd_dir/trojan-go/server.json ~/web/trojan-go
+
+# copy cloudreve config
+cp $cwd_dir/cloudreve/* ~/web/cloudreve
+
+# start cloudreve
+supervisorctl reload
 
